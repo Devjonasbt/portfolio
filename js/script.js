7d7ba8011a44f974a20aed97857010b9860c56a2
Devjonasbt <jonas.bcruz@senacsp.edu.br>
2023-07-25 22:17:58 -0300

    m

--- js/script.js
+++ js/script.js
@@ -18,6 +18,6 @@ const typed =new Typed('.texto',{
     strings:['Backend Developer', 'Frontend Developer', 'Analista de Sistemas'],
     typeSpeed: 20,
     backSpeed: 20,
-    backDelay: 1000,
+    backDelay: 1300,
     loop:true
 });
